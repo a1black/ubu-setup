24bbb0b31042dabed2f5c325477d6c8e7bd28e16
a1black <37011601+a1black@users.noreply.github.com>
2018-03-14 16:57:25 +0300

    Powerline.sh add option of updating python modules.

--- scripts/powerline.sh
+++ scripts/powerline.sh
@@ -4,20 +4,15 @@
 function show_usage() {
     cat << EOF
 Usage: $(basename $0) [OPTION]
-Install status line plugin Powerline.
+Install status line plugin Powerline for current user.
 OPTION:
     -u      User for whom Powerline will be installed.
-    -D      Print commands, don't execute them.
     -h      Show this message.
 
 EOF
     exit 1
 }
 
-function _eval() {
-    echo "$1"; [ -z "$UBU_SETUP_DRY" ] && eval "$1";
-    return $?
-}
 function _exit () {
     echo "Error: $1";
     echo "       Abort Powerline plugin installation."
@@ -25,27 +20,66 @@ function _exit () {
 }
 
 # Get available python version.
-function py_version() {
-    local version=$(python3 -c "import sys; print(sys.version_info[0])" 2> /dev/null)
-    if [ $? -ne 0 ]; then
-        version=$(python -c "import sys; print(sys.version_info[0])" 2> /dev/null)
-        if [ $? -ne 0 ]; then
-            return 127
+function get_python_version() {
+    for py_version in ${PYTHON_VERSIONS[@]}; do
+        if python$py_version -V > /dev/null 2>&1; then
+            python$py_version -c "import sys; print(sys.version_info[0])" 2> /dev/null
+            return 0
         fi
+    done
+    return 127
+}
+
+# Get powerline module installation path for provided python version.
+function get_python_module_path() {
+    python$1 -m pip show powerline-status | grep --color=never -oP '(?<=^Location: ).*'
+    return $?
+}
+
+# Install/update powerline module and dependencies.
+# Args:
+#   $1  Python version.
+#   $2  User name.
+#   $3  Upgrade flag.
+function install_update_powerline() {
+    [ "$3" = 'upgrade' ] && upgrade_flag='--upgrade' || upgrade_flag=''
+    if [ "$2" = 'root' ]; then
+        sudo python$1 -m pip instal -qq $upgrade_flag psutil netifaces powerline-status powerline-gitstatus
+    else
+        sudo su - $2 -c "python$1 -m pip install --user -qq $upgrade_flag psutil netifaces powerline-status powerline-gitstatus"
     fi
-    echo $(($version))
+    return $?
+}
+
+# Create symbol link to directory that contains plugins to bash/tmux/etc.
+# Symlink is made to speed up Powerline.
+function create_symlink() {
+    if [[ -z "$1" || -z "$2" || "$2" = 'root' ]]; then
+        exit 1
+    fi
+    local symlink=/home/$2/.local/lib/powerline-plugins
+    if [ ! -e $(dirname $symlink) ]; then
+        mkdir -p $(dirname $symlink)
+        chown $cuser:$(id -gn $cuser) $(dirname $symlink)
+    fi
+    ln -fns $1/powerline/bindings $symlink 2> /dev/null
+    if [ $? -ne 0 ]; then
+        rm -f $symlink 2> /dev/null
+        return 1
+    fi
+    chown $cuser:$(id -gn $cuser) $symlink
     return 0
 }
 
-# Default values.
+# Default and global values.
+PYTHON_VERSIONS=(3 2)
 [ -n "$SUDO_USER" ] && cuser="$SUDO_USER" || cuser="$USER"
 
 # Process arguments.
-while getopts ":hDu:" OPTION; do
+while getopts ":hu:" OPTION; do
     case $OPTION in
         u) cuser=$(id -nu "$OPTARG" 2> /dev/null);
             [ $? -ne 0 ] && _exit "Invalid user \"$OPTARG\".";;
-        D) UBU_SETUP_DRY=1;;
         h) show_usage;;
     esac
 done
@@ -55,30 +89,40 @@ if [[ $EUID -ne 0 && $cuser != $USER ]]; then
     _exit "Run script with root privileges."
 fi
 
-# Check if Powerline plugin is already installed.
-which powerline > /dev/null 2>&1
-if [ $? -eq 0 ]; then
-    _exit "Powerline plugin is already installed."
-fi
-
-# Check if `python` is installed.
-pyv=$(py_version)
-if [[ $? -ne 0 || $pyv -eq 0 ]]; then
-    _exit "Powerline plugin requires python."
-fi
-
-# Check if `pip` is installed.
-pyv="python$pyv"
-$pyv -m pip -V > /dev/null 2>&1
+# Available python version.
+pyv=$(get_python_version)
+python$pyv -m pip --version > /dev/null 2>&1
 if [ $? -ne 0 ]; then
-    _exit "Installer requires PIP module."
+    _exit "Python and PIP are required for Powerline installation."
+fi
+
+# Check if Powerline plugin is already installed.
+if powerline -h > /dev/null 2>&1 || [ -x /home/$cuser/.local/bin/powerline ]; then
+    echo "==> Update Powerline python module."
+    for py_version in ${PYTHON_VERSIONS[@]}; do
+        module_path=$(get_python_module_path $py_version)
+        [ $? -ne 0 ] && continue
+        if [[ "$module_path" =~ /home/$cuser/.local ]]; then
+            install_update_powerline $py_version $cuser 'upgrade'
+            create_symlink "$module_path" "$cuser"
+        else
+            install_update_powerline $py_version 'root' 'upgrade'
+        fi
+    done
+    exit 0
 fi
 
 # Install dependencies and powerline python module.
 echo "==> Install statusline plugin Powerline."
-#_eval "sudo apt-get install -qq libgit2-[0-9]"
-#_eval "sudo apt-get install -qq $pyv-pygit2"
-_eval "sudo su - $cuser -c '$pyv -m pip install --user -qq psutil netifaces powerline-status powerline-gitstatus'"
+#sudo apt-get install -qq libgit2-[0-9]
+#sudo apt-get install -qq python-pygit2
+install_update_powerline $pyv $cuser
+if [ $? -ne 0 ]; then
+    exit 1
+else
+    module_path=$(get_python_module_path $pyv)
+    create_symlink "$module_path" "$cuser"
+fi
 
 # Check if curl is installed
 curl --version > /dev/null 2>&1
@@ -86,44 +130,49 @@ HAS_CURL=$?
 
 # Install powerline fonts.
 echo "==> Download and install powerline fonts."
-fontdir="/home/$cuser/.local/share/fonts"
-fontconfdir="/home/$cuser/.config/fontconfig/conf.d"
+if [[ "$cuser" = 'root' ]]; then
+    fontdir=/usr/local/share/fonts
+    fontconfdir=/usr/share/fontconfig/conf.avail/
+else
+    fontdir="/home/$cuser/.local/share/fonts"
+    fontconfdir="/home/$cuser/.config/fontconfig/conf.d"
+fi
 symbols_github="https://github.com/powerline/powerline/raw/develop/font"
 fonts_github="https://github.com/powerline/fonts/raw/master"
 
-_eval "mkdir -p $fontdir $fontconfdir"
+mkdir -p $fontdir $fontconfdir
 # Fonts.
-fonts=("${symbols_github}/PowerlineSymbols.otf" \
-    "${fonts_github}/AnonymousPro/Anonymice Powerline Bold Italic.ttf" \
-    "${fonts_github}/AnonymousPro/Anonymice Powerline Bold.ttf" \
-    "${fonts_github}/AnonymousPro/Anonymice Powerline Italic.ttf" \
-    "${fonts_github}/AnonymousPro/Anonymice Powerline.ttf" \
-    "${fonts_github}/FiraMono/FuraMono-Regular Powerline.otf" \
-    "${fonts_github}/FiraMono/FuraMono-Medium Powerline.otf")
-fconfigs=("${symbols_github}/10-powerline-symbols.conf")
+fonts=("$symbols_github/PowerlineSymbols.otf" \
+    "$fonts_github/AnonymousPro/Anonymice Powerline Bold Italic.ttf" \
+    "$fonts_github/AnonymousPro/Anonymice Powerline Bold.ttf" \
+    "$fonts_github/AnonymousPro/Anonymice Powerline Italic.ttf" \
+    "$fonts_github/AnonymousPro/Anonymice Powerline.ttf" \
+    "$fonts_github/FiraMono/FuraMono-Regular Powerline.otf" \
+    "$fonts_github/FiraMono/FuraMono-Medium Powerline.otf")
+fconfigs=("$symbols_github/10-powerline-symbols.conf")
 
 # Download fonts.
 for ((i=0; i<${#fonts[@]}; i++)); do
     fontfile=$(basename "${fonts[$i]}")
     if [ $HAS_CURL -eq 0 ]; then
-        _eval "curl -sfLo \"${fontdir}/${fontfile}\" --create-dir \"${fonts[$i]}\""
+        curl -sfLo "$fontdir/$fontfile" "${fonts[$i]}"
     else
-        _eval "wget -qO - \"${fonts[$i]}\" > \"${fontdir}/${fontfile}\""
+        wget -qO - "${fonts[$i]}" > "$fontdir/$fontfile"
     fi
 done
 for ((i=0; i<${#fconfigs[@]}; i++)); do
     fconffile=$(basename "${fconfigs[$i]}")
     if [ $HAS_CURL -eq 0 ]; then
-        _eval "curl -sfLo \"${fontconfdir}/${fconffile}\" --create-dir \"${fconfigs[$i]}\""
+        curl -sfLo "$fontconfdir/$fconffile" "${fconfigs[$i]}"
     else
-        _eval "wget -qO - \"${fconfigs[$i]}\" > \"${fontconfdir}/${fconffile}\""
+        wget -qO - "${fconfigs[$i]}" > "$fontconfdir/$fconffile"
     fi
 done
 
 # Change owner of font directory.
-_eval "sudo chown -R ${cuser}:$(id -gn $cuser) $fontdir $fontconfdir"
+chown -R $cuser:$(id -gn $cuser) $fontdir $fontconfdir
 
 # Update font cache.
-if which fc-cache > /dev/null 2>&1; then
-    _eval "sudo fc-cache -vf $fontdir"
+if fc-cache --version > /dev/null 2>&1; then
+    sudo fc-cache -vf $fontdir
 fi
